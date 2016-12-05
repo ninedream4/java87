3af57519b9f8377c36a97d5453b71ac540e1cd09
slipknot999 <qoaqkrtk@naver.com>
2016-12-05 17:35:29 +0900

    LHS_1205_1

--- team2/bitShare/src/main/webapp/js/userValidationCheck.js
+++ team2/bitShare/src/main/webapp/js/userValidationCheck.js
@@ -1,18 +1,17 @@
-$.validator.setDefaults({
-	onkeyup:true
-});
+$(document).ready(function() {
 
-$(document).ready(function(){
-	$("#addUserForm").validate({
+	$("#addUserForm").validate({		
+		onkeyup:false,
 		rules:{
-			email: {required:true, email:true},
+			email: {required:true, email:true	},
 			pwd:{ required:true},
 			confirmPwd:{required:true, equalTo:"#pwd"}				
 			},
 		messages :{
 				email:{
 					    required:"Email을 입력하세요",
-			            email:"Email 형식이 아닙니다"
+			            email:"Email 형식이 아닙니다",
+			             /*remote : jQuery.format("{0}는 이미 있는 아이디입니다.")*/
 				},
 				 pwd:{
 					 required:"패스워드를 입력하세요"			            
@@ -22,6 +21,26 @@ $(document).ready(function(){
 		            equalTo:"패스워드가 다릅니다"
 		        }
 	
+		},
+		submitHandler: function(frm){
+			frm.submit();
 		}
 	});
+	$('#email').on('keyup', function() {
+	    var re = /([A-Z0-9a-z_-][^@])+?@[^$#<>?]+?\.[\w]{2,4}/.test(this.value);
+	    if(!re) {
+	        $('#errorEmail').show();
+	    } else {
+	        $('#errorEmail').hide();
+	    }
+	}),
+	$("#confirmPwd").keyup(function(event){
+		var pwd = $("#pwd").val();
+		if (pwd==$(this).val()){
+			  $('#errorPwd').hide();
+		}else{
+			  $('#errorPwd').show();
+		}		
+	});
+	
 });
